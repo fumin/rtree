d96849984f2ec04baa6975364c37e0cf38c74975
awaw <awaw@nandalu.idv.tw>
2013-08-31 20:26:50 +0800

    add comments

--- rtree_rpc.go
+++ rtree_rpc.go
@@ -1,3 +1,7 @@
+// RPC server and client functions to operate on Rtrees
+// It is encouraged to use these RPC functions instead of
+// doing direct RPC calls to increase type safety and avoid bugs with
+// string manipulation.
 package rtree
 
 import (
@@ -11,15 +15,8 @@ type server_t struct {
 	net.Listener
 }
 
-func (s *server_t) LoopAccept() error {
-	for {
-		conn, err := s.Accept()
-		if err != nil {
-			return err
-		}
-		go rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
-	}
-}
+// Creates a new server that listens on the network netp and address laddr.
+// To start the forever running Accept loop, call LoopAccept.
 func NewServer(netp, laddr string) (*server_t, error) {
 	store := NewStore()
 	rpc.Register(store)
@@ -31,10 +28,26 @@ func NewServer(netp, laddr string) (*server_t, error) {
 	return &server_t{l}, nil
 }
 
+// Starts the forever running Accept loop. If the loop is halted
+// due to an error, that error is returned.
+func (s *server_t) LoopAccept() error {
+  for {
+    conn, err := s.Accept()
+    if err != nil {
+      return err
+    }
+    go rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
+  }
+}
+
 type Client struct {
 	*rpc.Client
 }
 
+// Creates a new client connecting to the network netp and address laddr.
+// The returned client is simply a thin wrapper around rpc.Client, and thus
+// according to http://golang.org/pkg/net/rpc/#Client supports
+// concurrent requests and may be used by multiple goroutines simultaneously.
 func NewClient(netp, laddr string) (*Client, error) {
 	conn, err := net.Dial(netp, laddr)
 	if err != nil {
@@ -43,7 +56,12 @@ func NewClient(netp, laddr string) (*Client, error) {
 	return &Client{jsonrpc.NewClient(conn)}, nil
 }
 
-// RtreeInsert
+// Inserts a member into the Rtree identified by `key`.
+// The rectangle associated with `member` is defined to be
+// having the bottom corner `point` with lengths on all its dimensions
+// given by `lengths`.
+// Under this definition len(point) == len(lengths) and
+// all entries of `lengths` should be positive.
 func (c *Client) RtreeInsert(key, member string,
 	point, lengths []float64) error {
 	args, err := NewRtreeInsertArgs(key, member, point, lengths)
@@ -57,12 +75,14 @@ func (c *Client) RtreeInsert(key, member string,
 	}
 	return nil
 }
+// Asynchronous version of RtreeInsert.
+// Usage is similar to http://golang.org/pkg/net/rpc/#Client.Go
 func (c *Client) RtreeInsertGo(args *RtreeInsertArgs) *rpc.Call {
 	var reply RtreeInsertReply
 	return c.Go("Store.RtreeInsert", args, &reply, nil)
 }
 
-// RtreeDelete
+// Deletes a member from the Rtree identified by key.
 func (c *Client) RtreeDelete(key, member string) error {
 	reply := new(string)
 	err := c.Call("Store.RtreeDelete", &RtreeDeleteArgs{key, member}, reply)
@@ -71,12 +91,14 @@ func (c *Client) RtreeDelete(key, member string) error {
 	}
 	return nil
 }
+// Asynchronous version of RtreeDelete.
 func (c *Client) RtreeDeleteGo(key, member string) *rpc.Call {
 	reply := new(string)
 	return c.Go("Store.RtreeDelete", &RtreeDeleteArgs{key, member}, reply, nil)
 }
 
-// RtreeNearestNeighbors
+// Finds the k nearest neighbors around the point p in the Rtree
+// identified by key.
 func (c *Client) RtreeNearestNeighbors(key string, k int, p rtreego.Point) ([]string, error) {
 	args := &RtreeNearestNeighborsArgs{key, k, p}
 	reply := new(RtreeNearestNeighborsReply)
@@ -86,6 +108,7 @@ func (c *Client) RtreeNearestNeighbors(key string, k int, p rtreego.Point) ([]st
 	}
 	return reply.Members, nil
 }
+// Asynchronous version of RtreeNearestNeighbors.
 func (c *Client) RtreeNearestNeighborsGo(key string, k int, p rtreego.Point) *rpc.Call {
 	args := &RtreeNearestNeighborsArgs{key, k, p}
 	reply := new(RtreeNearestNeighborsReply)
